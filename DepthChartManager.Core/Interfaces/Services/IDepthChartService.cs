7da5f6e2775e27bec9da787f6c6178624d8d054b
Narendrakumar Sethuraman <47780831+narendotnet@users.noreply.github.com>
2022-07-31 21:46:20 +0930

    Add files via upload

---
+++ DepthChartManager.Core/Interfaces/Services/IDepthChartService.cs
@@ -0,0 +1,16 @@
+﻿using DepthChartManager.Core.Dtos.Response;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DepthChartManager.Core.Interfaces.Services
+{
+    public interface IDepthChartService
+    {
+        Task<LeagueDto> AddLeague(int leagueId, string leagueName);
+        Task<TeamDto> AddTeam(int id,int leagueId, string teamName);
+        Task<PlayerDto> AddPlayerToDepthChart(int playerId, int leagueId, int teamId, string playerName, string position, int? positionDepth);
+        Task<PlayerDto> RemovePlayerFromDepthChart(int playerId, int leagueId, int teamId, string name, string position);
+        Task<IEnumerable<PlayerDto>> GetBackups(int playerId, int leagueId, int teamId, string name, string position);
+        Task<IEnumerable<PlayerDto>> GetFullDepthChart(int leagueId, int teamId);
+    }
+}
