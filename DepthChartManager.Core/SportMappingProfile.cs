4071205f90d30f6a60dbbd17e7004bd4d6c37c82
Narendrakumar Sethuraman <47780831+narendotnet@users.noreply.github.com>
2022-07-31 21:41:51 +0930

    Add files via upload

---
+++ DepthChartManager.Core/SportMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DepthChartManager.Core.Dtos.Response;
+using DepthChartManager.Domain;
+
+namespace DepthChartManager.Core
+{
+    public class SportMappingProfile : Profile
+    {
+        public SportMappingProfile()
+        {
+            CreateMap<League, LeagueDto>();
+            CreateMap<Player, PlayerDto>();
+            CreateMap<Team, TeamDto>();
+        }
+    }
+}
