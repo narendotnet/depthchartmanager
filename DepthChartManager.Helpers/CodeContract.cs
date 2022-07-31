7fa34b3c9f32dc5a86ccd7d07e98c4590cfa2a87
Narendrakumar Sethuraman <47780831+narendotnet@users.noreply.github.com>
2022-07-31 21:50:03 +0930

    Add files via upload

---
+++ DepthChartManager.Helpers/CodeContract.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DepthChartManager.Helpers
+{
+    public static class Contract
+    {
+        public static void Requires<TException>(bool condition, string message) where TException : Exception
+        {
+            if (!condition) throw Activator.CreateInstance(typeof(TException), message) as TException;
+        }
+    }
+}
