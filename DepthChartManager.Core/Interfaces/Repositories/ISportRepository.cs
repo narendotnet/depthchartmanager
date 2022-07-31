c9d6b5faa45251a1b3e65efe49f3875238e9c141
Narendrakumar Sethuraman <47780831+narendotnet@users.noreply.github.com>
2022-07-31 21:45:51 +0930

    Add files via upload

---
+++ DepthChartManager.Core/Interfaces/Repositories/ISportRepository.cs
@@ -0,0 +1,17 @@
+﻿using DepthChartManager.Domain;
+using System.Collections.Generic;
+
+namespace DepthChartManager.Core.Interfaces.Repositories
+{
+    public interface ISportRepository
+    {
+        League AddLeague(int id, string name);
+        //IEnumerable<League> GetLeagues();
+        Team AddTeam(int leagueId, string teamName);
+        //IEnumerable<Team> GetTeams(int leagueId);
+        Player AddPlayer(int id, int leagueId, int teamId, string name, string position, int? positionDepth);
+        Player RemovePlayer(int id, int leagueId, int teamId, string name, string position);
+        IEnumerable<Player> GetBackups(int playerId, int leagueId, int teamId, string name, string position);
+        IEnumerable<Player> GetFullDepthChart(int leagueId, int teamId);
+    }
+}
